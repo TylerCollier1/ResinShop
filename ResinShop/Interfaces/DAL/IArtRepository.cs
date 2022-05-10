cfaa3e2e408618cbc39612c9845c2da74af272b6
Tyler Collier <tcollier@dev-10.com>
2022-05-10 13:16:02 -0500

    Added variables

--- ResinShop/Interfaces/DAL/IArtRepository.cs
+++ ResinShop/Interfaces/DAL/IArtRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResinShop.Core.Entities;
 
 namespace ResinShop.Core.Interfaces.DAL
 {
     public interface IArtRepository
     {
+        Response<Art> Insert(Art art);
+        Response Update(Art art);
+        Response Delete(int artId);
+        Response<Art> Get(int artId);
     }
 }
