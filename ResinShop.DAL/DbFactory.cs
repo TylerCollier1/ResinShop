e9e849064e5428dbdc7a4c244160ca2795144d45
Tyler Collier <tcollier@dev-10.com>
2022-05-11 10:36:18 -0500

    Filled in these files.

--- ResinShop.DAL/DbFactory.cs
+++ ResinShop.DAL/DbFactory.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ResinShop.DAL
 {
+    public enum FactoryMode
+    {
+        TEST,
+        PROD
+    }
     public class DbFactory
     {
+        public static DbContextOptions GetDbContext(FactoryMode mode)
+        {
+            string environment = mode == FactoryMode.TEST ? "Test" : "Live";
+
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets<ConfigProvider>();
+            var config = builder.Build();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer(config[$"ConnectionStrings:{environment}"])
+                .Options;
+            return options;
+        }
     }
 }
