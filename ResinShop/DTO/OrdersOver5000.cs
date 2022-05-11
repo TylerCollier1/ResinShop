6fdc0ae5e7da2c804994d3065ca9393d6d932a40
Lindsey Bremer <Lbremer@dev-10.com>
2022-05-11 14:25:12 -0500

    Renamed Reports DTO's Did Reports repository and interface

---
+++ ResinShop/DTO/OrdersOver5000.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResinShop.Core.DTO
+{
+    public class OrdersOver5000
+    {
+        public int OrderId { get; set; }
+        public int ArtId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
--- ResinShop/Interfaces/DAL/IReportsRepository.cs
+++ ResinShop/Interfaces/DAL/IReportsRepository.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ResinShop.Core.DTO;
 using ResinShop.Core.Entities;
 
 namespace ResinShop.Core.Interfaces.DAL
 {
     public interface IReportsRepository
     {
-        //Response<List<TopAgentListItem>> GetTopAgents();
-        //Response<List<PensionListItem>> GetPensionList(int agencyId);
-        //Response<List<ClearanceAuditListItem>> AuditClearance(int securityClearanceId, int agencyId);
+        Response<List<OrdersOver5000>> GetOrdersOver5000();
+        Response<List<LargeArtPieces>> GetLargeArtPieces();
+        Response<List<HasAdvancedFeatures>> GetAdvancedFeatures();
     }
 }
