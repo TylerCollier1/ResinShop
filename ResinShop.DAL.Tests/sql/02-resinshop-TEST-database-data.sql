ac8b2b6e01015f7a4e91c39151aa0daf801ec2fd
Toi Parker <tparker@dev-10.com>
2022-05-13 18:25:05 -0400

    update sql and csv files

---
+++ ResinShop.DAL.Tests/sql/02-resinshop-TEST-database-data.sql
@@ -0,0 +1,59 @@
+--Customer
+INSERT INTO Customer (FirstName, LastName, Email, StreetAddress, City, StateName, ZipCode, PhoneNumber)
+    VALUES
+    ('Adeline', 'Fulcher', 'afulcher4@tiny.cc', '58164 Algoma Hill', 'Cleveland', 'OH', 44105, 2164319017),
+    ('Allix', 'Brett', 'abrett5@marriott.com', '7533 Truax Alley', 'Flushing', 'NY', 11388, 3476959606),
+    ('Bastien', 'Pusill', 'bpusillj@dion.ne.jp', '404 Trailsway Circle',	'Sacramento', 'CA',	9581391, 66977842);
+
+--Adv Feature
+INSERT INTO AdvancedFeature (AdvancedFeatureName)
+    VALUES
+    ('2-piece'),
+    ('3-piece'),
+    ('floating gem'),
+    ('none');
+
+--Art
+INSERT INTO Art (Height, Width, MaterialQuantity, ColorQuantity, AdvancedFeatureId, Cost)
+    VALUES
+    (15.00, 23.00, 1, 1, 1, 948.75),
+    (35.00, 53.00, 1, 1, 2, 5101.25),
+    (55.00, 36.00, 1, 1, 3, 5445.00);
+
+--Order
+INSERT INTO [Order] (CustomerId, ArtId, OrderDate)
+    VALUES
+    (1, 1, '2021-07-02'),
+    (2, 2, '2021-08-03'),
+    (3, 3, '2021-09-04');
+
+--Material
+INSERT INTO Color (ColorName)
+    VALUES 
+    ('Red'),
+    ('Pink'),
+    ('White'),
+    ('Gold');
+
+--Color
+INSERT INTO Material (MaterialName)
+    VALUES
+    ('Glass'),
+    ('Gem'),
+    ('Shimmer'),
+    ('Stone');
+
+
+--ArtColor
+INSERT INTO ArtColor (ArtId, ColorId)
+    VALUES 
+    (1, 3),
+    (2,	1),
+    (3,	1);
+
+--ArtMaterial
+INSERT INTO ArtMaterial (ArtId, MaterialId)
+    VALUES
+    (1, 3),
+    (2,	2),
+    (3,	2);
